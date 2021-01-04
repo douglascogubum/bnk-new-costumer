4793d953f3992d3d32c86edca80057b4add12e57
Douglas Cogubum <doug.cogubum@gmail.com>
2021-01-04 16:14:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Spring Project
+
+Spring project based on Java 11 and MariaDB.
+
+Spring boot technologies used:
+
+* Spring Web
+* Spring Data
+* Spring Security
+* SpringFox Swagger
+
+Dependencies:
+* Lombok
+* MySQL Connector
+* Jacoco
+* Hamcrest
+
---
+++ src/main/java/com/zup/bank/BnkRegisterCostumerApplication.java
@@ -0,0 +1,16 @@
+package com.zup.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class BnkRegisterCostumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnkRegisterCostumerApplication.class, args);
+	}
+
+}
