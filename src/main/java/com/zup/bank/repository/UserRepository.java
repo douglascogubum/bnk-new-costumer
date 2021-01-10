834a2f36fe22195a32bb031ddd203fe7a14f4e7f
Douglas Cogubum <doug.cogubum@gmail.com>
2021-01-10 20:18:44 -0300

    Missing Repository annotation in UserRepository Class

--- src/main/java/com/zup/bank/repository/UserRepository.java
+++ src/main/java/com/zup/bank/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.zup.bank.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zup.bank.model.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 }
